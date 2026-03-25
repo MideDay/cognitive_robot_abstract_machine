0f6a03573189df1c05b5186c17ef3c90b03eb9cc
Sanic <sanic@users.noreply.github.com>
2026-03-25 22:19:36 +0100

    Changing tests to new Region format

--- robokudo/test/test_utils_semantic_map.py
+++ robokudo/test/test_utils_semantic_map.py
@@ -1,58 +1,55 @@
 import numpy as np
 import pytest
 
-@pytest.mark.skip(reason="Semantic maps are deprecated in favor of world descriptors.")
-class TestUtilsSemanticMap:
+from robokudo.utils.region import (
+    region_obb,
+    region_obb_in_cam_coordinates,
+    region_pose_annotation,
+)
+from robokudo.world_descriptor import BaseWorldDescriptor, RegionSpec
+from semantic_digital_twin.spatial_types import HomogeneousTransformationMatrix
+from semantic_digital_twin.world_description.geometry import Scale
+from semantic_digital_twin.world_description.world_entity import Region
+
+
+class TestUtilsRegion:
     @pytest.fixture
-    def region(self):
-        from robokudo.semantic_map import SemanticMapEntry
+    def world_and_region(self):
+        world_descriptor = BaseWorldDescriptor()
+        root = world_descriptor.world.root
+        spec = RegionSpec(
+            name="test_region",
+            box_scale=Scale(1.0, 2.0, 3.0),
+            pose=HomogeneousTransformationMatrix.from_xyz_quaternion(
+                pos_x=3.0,
+                pos_y=2.0,
+                pos_z=1.0,
+                quat_x=0.5,
+                quat_y=0.5,
+                quat_z=0.5,
+                quat_w=0.5,
+                reference_frame=root,
+            ),
+        )
+        world_descriptor.build_regions(root, [spec])
+        regions = world_descriptor.world.get_kinematic_structure_entity_by_type(Region)
+        assert len(regions) == 1
+        return world_descriptor.world, regions[0]
 
-        region = SemanticMapEntry()
-        region.x_size = 1.0
-        region.y_size = 2.0
-        region.z_size = 3.0
-        region.orientation_x = 0.5
-        region.orientation_y = 0.5
-        region.orientation_z = 0.5
-        region.orientation_w = 0.5
-        region.position_x = 3.0
-        region.position_y = 2.0
-        region.position_z = 1.0
-        return region
+    def test_region_obb(self, world_and_region):
+        world, region = world_and_region
+        obb = region_obb(region, world=world)
 
-    def test_get_obb_from_semantic_map_region(self, region):
-        from robokudo.utils.semantic_map import get_obb_from_semantic_map_region
-
-        obb = get_obb_from_semantic_map_region(region)
-
-        assert np.all(obb.extent == [1.0, 2.0, 3.0])
-        assert np.all(obb.center == [region.position_x, region.position_y, region.position_z])
+        assert np.allclose(np.asarray(obb.extent).reshape(-1)[:3], [1.0, 2.0, 3.0])
+        assert np.allclose(np.asarray(obb.center).reshape(-1)[:3], [3.0, 2.0, 1.0])
         assert np.allclose(obb.R, [
             [0.0, 0.0, 1.0],
             [1.0, 0.0, 0.0],
             [0.0, 1.0, 0.0]
         ])
 
-    def test_get_obb_from_semantic_map_region_in_cam_coordinates_camera_frame_region(self, region):
-        from robokudo.utils.semantic_map import get_obb_from_semantic_map_region_in_cam_coordinates
-
-        region.frame_id = "camera_link"
-        obb = get_obb_from_semantic_map_region_in_cam_coordinates(region, world_frame_name="map",
-                                                                  world_to_cam_transform_matrix=np.eye(3))
-
-        assert np.all(obb.extent == [1.0, 2.0, 3.0])
-        assert np.all(obb.center == [region.position_x, region.position_y, region.position_z])
-        assert np.allclose(obb.R, [
-            [0.0, 0.0, 1.0],
-            [1.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0]
-        ])
-
-    def test_get_obb_from_semantic_map_region_in_cam_coordinates_world_frame_region(self, region):
-        from robokudo.utils.semantic_map import get_obb_from_semantic_map_region_in_cam_coordinates
-
-        region.frame_id = "map"
-
+    def test_region_obb_in_cam_coordinates(self, world_and_region):
+        world, region = world_and_region
         world_to_cam = np.array([
             [0.0, 0.0, 1.0, 1.0],
             [-1.0, 0.0, 0.0, 2.0],
@@ -60,33 +57,27 @@ class TestUtilsSemanticMap:
             [0.0, 0.0, 0.0, 1.0]
         ])
 
-        obb = get_obb_from_semantic_map_region_in_cam_coordinates(region, world_frame_name="map",
-                                                                  world_to_cam_transform_matrix=world_to_cam)
+        obb = region_obb_in_cam_coordinates(
+            world=world, region=region, world_T_cam=world_to_cam
+        )
 
-        assert np.all(obb.extent == [1.0, 2.0, 3.0])
-        assert np.all(obb.center == [region.position_x - 1.0, region.position_y - 3.0, region.position_z])
+        assert np.allclose(np.asarray(obb.extent).reshape(-1)[:3], [1.0, 2.0, 3.0])
+        assert np.allclose(np.asarray(obb.center).reshape(-1)[:3], [2.0, -1.0, 1.0])
         assert np.allclose(obb.R, [
             [0.0, 1.0, 0.0],
             [0.0, 0.0, -1.0],
             [-1.0, 0.0, 0.0]
         ])
 
-    def test_get_obb_from_semantic_map_region_with_transform_matrix(self, region):
-        from robokudo.utils.semantic_map import get_obb_from_semantic_map_region_with_transform_matrix
+    def test_region_pose_annotation(self, world_and_region):
+        _, region = world_and_region
+        pose_annotation = region_pose_annotation(region)
 
-        world_to_cam = np.array([
-            [0.0, 0.0, 1.0, 1.0],
-            [-1.0, 0.0, 0.0, 2.0],
-            [0.0, -1.0, 0.0, 3.0],
-            [0.0, 0.0, 0.0, 1.0]
-        ])
-
-        obb = get_obb_from_semantic_map_region_with_transform_matrix(region, transform_matrix=world_to_cam)
-
-        assert np.all(obb.extent == [1.0, 2.0, 3.0])
-        assert np.all(obb.center == [region.position_x - 1.0, region.position_y - 3.0, region.position_z])
-        assert np.allclose(obb.R, [
-            [0.0, 1.0, 0.0],
-            [0.0, 0.0, -1.0],
-            [-1.0, 0.0, 0.0]
-        ])
+        assert np.allclose(
+            np.asarray(pose_annotation.translation).reshape(-1)[:3], [3.0, 2.0, 1.0]
+        )
+        # q and -q encode the same rotation, compare absolute values for robustness.
+        assert np.allclose(
+            np.abs(np.asarray(pose_annotation.rotation).reshape(-1)[:4]),
+            [0.5, 0.5, 0.5, 0.5],
+        )
