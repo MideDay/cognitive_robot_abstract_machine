70f4735f01c048e59ec9c8a5b3f595ad1c5ad9c0
MideDay <l-hauschild@outlook.de>
2026-07-28 11:03:02 +0200

    testing ci on non-main branch

--- .github/docker/setup_workspace.py
+++ .github/docker/setup_workspace.py
@@ -187,7 +187,7 @@ def main():
     repos = [
         Repository("https://github.com/code-iai/iai_maps.git", "ros-jazzy", "iai_maps"),
         Repository(
-            "https://github.com/code-iai/iai_robots.git", "ros-jazzy", "iai_robots"
+            "https://github.com/MideDay/iai_robots.git", "ros-jazzy", "iai_robots"
         ),
         Repository(
             "https://github.com/code-iai/iai_pr2.git", "ros-jazzy-main", "iai_pr2"
